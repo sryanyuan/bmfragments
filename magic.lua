ca3a3371b71a292d9f37bf92fbbc82c779f69bb2
sryan yuan <sryan@qq.com>
2019-11-27 16:41:18 +0800

    Create magic.lua

---
+++ magic.lua
@@ -0,0 +1,1034 @@
+config_constMagicRecord = {
+	-- Zhanshi
+    [2] = {
+        Name = "基本剑术",
+        Desc = "",
+        Passive = 1,
+        Job = 0,
+        Colldown = 0,
+        Increase = 0,
+        Multiple = 0,
+        Lvs = {
+            {
+                LvReq = 7,
+                Cost = 0,
+            },
+			{
+                LvReq = 9,
+                Cost = 0,
+            },
+			{
+                LvReq = 12,
+                Cost = 0,
+            }
+        }
+    },
+	[3] = {
+        Name = "刺杀",
+        Desc = "",
+        Passive = 0,
+        Job = 0,
+        Colldown = 0,
+        Increase = 0,
+        Multiple = 0,
+        Lvs = {
+            {
+                LvReq = 25,
+                Cost = 0,
+            },
+			{
+                LvReq = 27,
+                Cost = 0,
+            },
+			{
+                LvReq = 29,
+                Cost = 0,
+            },
+			{
+                LvReq = 50,
+                Cost = 0,
+            },
+			{
+                LvReq = 52,
+                Cost = 0,
+            },
+			{
+                LvReq = 54,
+                Cost = 0,
+            }
+        }
+    },
+	[4] = {
+        Name = "攻杀",
+        Desc = "",
+        Passive = 1,
+        Job = 0,
+        Colldown = 0,
+        Increase = 0,
+        Multiple = 0,
+        Lvs = {
+            {
+                LvReq = 19,
+                Cost = 0,
+            },
+			{
+                LvReq = 22,
+                Cost = 0,
+            },
+			{
+                LvReq = 24,
+                Cost = 0,
+            }
+        }
+    },
+	[5] = {
+        Name = "半月",
+        Desc = "",
+        Passive = 0,
+        Job = 0,
+        Colldown = 0,
+        Increase = 0,
+        Multiple = 0,
+        Lvs = {
+            {
+                LvReq = 28,
+                Cost = 2,
+            },
+			{
+                LvReq = 31,
+                Cost = 0,
+            },
+			{
+                LvReq = 34,
+                Cost = 0,
+            }
+        }
+    },
+	[6] = {
+        Name = "烈火",
+        Desc = "",
+        Passive = 0,
+        Job = 0,
+        Colldown = 0,
+        Increase = 0,
+        Multiple = 0,
+        Lvs = {
+            {
+                LvReq = 35,
+                Cost = 10,
+            },
+			{
+                LvReq = 37,
+                Cost = 0,
+            },
+			{
+                LvReq = 40,
+                Cost = 0,
+            }
+        }
+    },
+	[7] = {
+        Name = "稳如泰山",
+        Desc = "",
+        Passive = 1,
+        Job = 0,
+        Colldown = 0,
+        Increase = 0,
+        Multiple = 0,
+        Lvs = {
+            {
+                LvReq = 33,
+                Cost = 0,
+            },
+			{
+                LvReq = 36,
+                Cost = 0,
+            },
+			{
+                LvReq = 40,
+                Cost = 0,
+            }
+        }
+    },
+	[8] = {
+        Name = "狂战士",
+        Desc = "",
+        Passive = 1,
+        Job = 0,
+        Colldown = 0,
+        Increase = 0,
+        Multiple = 0,
+        Lvs = {
+            {
+                LvReq = 30,
+                Cost = 0,
+            },
+			{
+                LvReq = 35,
+                Cost = 0,
+            },
+			{
+                LvReq = 42,
+                Cost = 0,
+            }
+        }
+    },
+	[9] = {
+        Name = "狮子吼",
+        Desc = "",
+        Passive = 0,
+        Job = 0,
+        Colldown = 5000,
+        Increase = 0,
+        Multiple = 0,
+        Lvs = {
+            {
+                LvReq = 40,
+                Cost = 10,
+            },
+			{
+                LvReq = 42,
+                Cost = 0,
+            },
+			{
+                LvReq = 44,
+                Cost = 0,
+            }
+        }
+    },
+	[10] = {
+        Name = "炙炎剑法",
+        Desc = "",
+        Passive = 0,
+        Job = 0,
+        Colldown = 0,
+        Increase = 0,
+        Multiple = 0,
+        Lvs = {
+            {
+                LvReq = 42,
+                Cost = 16,
+            },
+			{
+                LvReq = 44,
+                Cost = 0,
+            },
+			{
+                LvReq = 46,
+                Cost = 0,
+            }
+        }
+    },
+	[11] = {
+        Name = "开天斩",
+        Desc = "",
+        Passive = 1,
+        Job = 0,
+        Colldown = 0,
+        Increase = 0,
+        Multiple = 0,
+        Lvs = {
+            {
+                LvReq = 44,
+                Cost = 0,
+            },
+			{
+                LvReq = 46,
+                Cost = 0,
+            },
+			{
+                LvReq = 48,
+                Cost = 0,
+            }
+        }
+    },
+	[12] = {
+        Name = "血龙剑法",
+        Desc = "",
+        Passive = 0,
+        Job = 0,
+        Colldown = 0,
+        Increase = 0,
+        Multiple = 0,
+        Lvs = {
+            {
+                LvReq = 50,
+                Cost = 20,
+            },
+			{
+                LvReq = 53,
+                Cost = 0,
+            },
+			{
+                LvReq = 55,
+                Cost = 0,
+            }
+        }
+    },
+	[12] = {
+        Name = "金刚不坏",
+        Desc = "",
+        Passive = 0,
+        Job = 0,
+        Colldown = 0,
+        Increase = 0,
+        Multiple = 0,
+        Lvs = {
+            {
+                LvReq = 55,
+                Cost = 26,
+            },
+			{
+                LvReq = 57,
+                Cost = 0,
+            },
+			{
+                LvReq = 59,
+                Cost = 0,
+            }
+        }
+    },
+	
+	-- Fashi
+	[29] = {
+        Name = "小火球",
+        Desc = "",
+        Passive = 0,
+        Job = 1,
+        Colldown = 0,
+        Increase = 2,
+        Multiple = 15,
+        Lvs = {
+            {
+                LvReq = 7,
+                Cost = 4,
+            },
+			{
+                LvReq = 9,
+                Cost = 0,
+            },
+			{
+                LvReq = 11,
+                Cost = 0,
+            }
+        }
+    },
+	[30] = {
+        Name = "大火球",
+        Desc = "",
+        Passive = 0,
+        Job = 1,
+        Colldown = 0,
+        Increase = 4,
+        Multiple = 20,
+        Lvs = {
+            {
+                LvReq = 19,
+                Cost = 8,
+            },
+			{
+                LvReq = 21,
+                Cost = 0,
+            },
+			{
+                LvReq = 23,
+                Cost = 0,
+            }
+        }
+    },
+	[31] = {
+        Name = "爆裂火焰",
+        Desc = "",
+        Passive = 0,
+        Job = 1,
+        Colldown = 0,
+        Increase = 4,
+        Multiple = 15,
+        Lvs = {
+            {
+                LvReq = 22,
+                Cost = 14,
+            },
+			{
+                LvReq = 24,
+                Cost = 0,
+            },
+			{
+                LvReq = 27,
+                Cost = 0,
+            }
+        }
+    },
+	[33] = {
+        Name = "冰咆哮",
+        Desc = "",
+        Passive = 0,
+        Job = 1,
+        Colldown = 0,
+        Increase = 10,
+        Multiple = 24,
+        Lvs = {
+            {
+                LvReq = 35,
+                Cost = 22,
+            },
+			{
+                LvReq = 37,
+                Cost = 0,
+            },
+			{
+                LvReq = 40,
+                Cost = 0,
+            }
+        }
+    },
+	[34] = {
+        Name = "地狱雷光",
+        Desc = "",
+        Passive = 0,
+        Job = 1,
+        Colldown = 0,
+        Increase = 5,
+        Multiple = 15,
+        Lvs = {
+            {
+                LvReq = 30,
+                Cost = 20,
+            },
+			{
+                LvReq = 33,
+                Cost = 0,
+            },
+			{
+                LvReq = 36,
+                Cost = 0,
+            }
+        }
+    },
+	[35] = {
+        Name = "雷电术",
+        Desc = "",
+        Passive = 0,
+        Job = 1,
+        Colldown = 0,
+        Increase = 8,
+        Multiple = 27,
+        Lvs = {
+            {
+                LvReq = 17,
+                Cost = 8,
+            },
+			{
+                LvReq = 19,
+                Cost = 0,
+            },
+			{
+                LvReq = 22,
+                Cost = 0,
+            },
+			{
+                LvReq = 50,
+                Cost = 0,
+            },
+			{
+                LvReq = 52,
+                Cost = 0,
+            },
+			{
+                LvReq = 54,
+                Cost = 0,
+            }
+        }
+    },
+	[36] = {
+        Name = "火墙",
+        Desc = "",
+        Passive = 0,
+        Job = 1,
+        Colldown = 0,
+        Increase = 6,
+        Multiple = 15,
+        Lvs = {
+            {
+                LvReq = 24,
+                Cost = 26,
+            },
+			{
+                LvReq = 28,
+                Cost = 0,
+            },
+			{
+                LvReq = 32,
+                Cost = 0,
+            }
+        }
+    },
+	[37] = {
+        Name = "魔法盾",
+        Desc = "",
+        Passive = 0,
+        Job = 1,
+        Colldown = 0,
+        Increase = 0,
+        Multiple = 0,
+        Lvs = {
+            {
+                LvReq = 33,
+                Cost = 18,
+            },
+			{
+                LvReq = 35,
+                Cost = 0,
+            },
+			{
+                LvReq = 37,
+                Cost = 0,
+            },
+			{
+                LvReq = 55,
+                Cost = 18,
+            },
+			{
+                LvReq = 58,
+                Cost = 0,
+            },
+			{
+                LvReq = 62,
+                Cost = 0,
+            }
+        }
+    },
+	[38] = {
+        Name = "诱惑之光",
+        Desc = "",
+        Passive = 0,
+        Job = 1,
+        Colldown = 0,
+        Increase = 0,
+        Multiple = 0,
+        Lvs = {
+            {
+                LvReq = 13,
+                Cost = 12,
+            },
+			{
+                LvReq = 15,
+                Cost = 0,
+            },
+			{
+                LvReq = 18,
+                Cost = 0,
+            }
+        }
+    },
+	[39] = {
+        Name = "寒冰掌",
+        Desc = "",
+        Passive = 0,
+        Job = 1,
+        Colldown = 1000,
+        Increase = 5,
+        Multiple = 10,
+        Lvs = {
+            {
+                LvReq = 40,
+                Cost = 14,
+            },
+			{
+                LvReq = 42,
+                Cost = 0,
+            },
+			{
+                LvReq = 44,
+                Cost = 0,
+            }
+        }
+    },
+	[40] = {
+        Name = "灭天火",
+        Desc = "",
+        Passive = 0,
+        Job = 1,
+        Colldown = 200,
+        Increase = 14,
+        Multiple = 35,
+        Lvs = {
+            {
+                LvReq = 42,
+                Cost = 16,
+            },
+			{
+                LvReq = 44,
+                Cost = 0,
+            },
+			{
+                LvReq = 46,
+                Cost = 0,
+            }
+        }
+    },
+	[41] = {
+        Name = "火流星",
+        Desc = "",
+        Passive = 0,
+        Job = 1,
+        Colldown = 4000,
+        Increase = 12,
+        Multiple = 26,
+        Lvs = {
+            {
+                LvReq = 44,
+                Cost = 34,
+            },
+			{
+                LvReq = 46,
+                Cost = 0,
+            },
+			{
+                LvReq = 48,
+                Cost = 0,
+            }
+        }
+    },
+	[42] = {
+        Name = "天霜冰环",
+        Desc = "",
+        Passive = 0,
+        Job = 1,
+        Colldown = 2000,
+        Increase = 12,
+        Multiple = 25,
+        Lvs = {
+            {
+                LvReq = 50,
+                Cost = 38,
+            },
+			{
+                LvReq = 53,
+                Cost = 0,
+            },
+			{
+                LvReq = 56,
+                Cost = 0,
+            }
+        }
+    },
+	[43] = {
+        Name = "火龙气焰",
+        Desc = "",
+        Passive = 0,
+        Job = 1,
+        Colldown = 2000,
+        Increase = 14,
+        Multiple = 30,
+        Lvs = {
+            {
+                LvReq = 62,
+                Cost = 45,
+            },
+			{
+                LvReq = 65,
+                Cost = 0,
+            },
+			{
+                LvReq = 68,
+                Cost = 0,
+            }
+        }
+    },
+	
+	-- Daoshi
+	[56] = {
+        Name = "治愈术",
+        Desc = "",
+        Passive = 0,
+        Job = 2,
+        Colldown = 0,
+        Increase = 5,
+        Multiple = 16,
+        Lvs = {
+            {
+                LvReq = 7,
+                Cost = 4,
+            },
+			{
+                LvReq = 9,
+                Cost = 0,
+            },
+			{
+                LvReq = 12,
+                Cost = 0,
+            }
+        }
+    },
+	[57] = {
+        Name = "灵魂火符",
+        Desc = "",
+        Passive = 0,
+        Job = 2,
+        Colldown = 0,
+        Increase = 4,
+        Multiple = 17,
+        Lvs = {
+            {
+                LvReq = 18,
+                Cost = 6,
+            },
+			{
+                LvReq = 21,
+                Cost = 0,
+            },
+			{
+                LvReq = 24,
+                Cost = 0,
+            },
+			{
+                LvReq = 53,
+                Cost = 0,
+            },
+			{
+                LvReq = 55,
+                Cost = 0,
+            },
+			{
+                LvReq = 57,
+                Cost = 0,
+            }
+        }
+    },
+	[58] = {
+        Name = "神圣战甲术",
+        Desc = "",
+        Passive = 0,
+        Job = 2,
+        Colldown = 0,
+        Increase = 0,
+        Multiple = 0,
+        Lvs = {
+            {
+                LvReq = 22,
+                Cost = 10,
+            },
+			{
+                LvReq = 24,
+                Cost = 0,
+            },
+			{
+                LvReq = 26,
+                Cost = 0,
+            }
+        }
+    },
+	[60] = {
+        Name = "隐身术",
+        Desc = "",
+        Passive = 0,
+        Job = 2,
+        Colldown = 0,
+        Increase = 0,
+        Multiple = 0,
+        Lvs = {
+            {
+                LvReq = 20,
+                Cost = 6,
+            },
+			{
+                LvReq = 22,
+                Cost = 0,
+            },
+			{
+                LvReq = 25,
+                Cost = 0,
+            }
+        }
+    },
+	[61] = {
+        Name = "精神力战法",
+        Desc = "",
+        Passive = 1,
+        Job = 2,
+        Colldown = 0,
+        Increase = 0,
+        Multiple = 0,
+        Lvs = {
+            {
+                LvReq = 9,
+                Cost = 0,
+            },
+			{
+                LvReq = 11,
+                Cost = 0,
+            },
+			{
+                LvReq = 14,
+                Cost = 0,
+            }
+        }
+    },
+	[62] = {
+        Name = "召唤术",
+        Desc = "",
+        Passive = 0,
+        Job = 2,
+        Colldown = 0,
+        Increase = 0,
+        Multiple = 0,
+        Lvs = {
+            {
+                LvReq = 35,
+                Cost = 32,
+            },
+			{
+                LvReq = 37,
+                Cost = 0,
+            },
+			{
+                LvReq = 40,
+                Cost = 0,
+            }
+        }
+    },
+	[63] = {
+        Name = "施毒术",
+        Desc = "",
+        Passive = 0,
+        Job = 2,
+        Colldown = 0,
+        Increase = 0,
+        Multiple = 0,
+        Lvs = {
+            {
+                LvReq = 14,
+                Cost = 8,
+            },
+			{
+                LvReq = 16,
+                Cost = 0,
+            },
+			{
+                LvReq = 18,
+                Cost = 0,
+            },
+			{
+                LvReq = 50,
+                Cost = 0,
+            },
+			{
+                LvReq = 52,
+                Cost = 0,
+            },
+			{
+                LvReq = 54,
+                Cost = 0,
+            }
+        }
+    },
+	[64] = {
+        Name = "召唤骷髅",
+        Desc = "",
+        Passive = 0,
+        Job = 2,
+        Colldown = 0,
+        Increase = 0,
+        Multiple = 0,
+        Lvs = {
+            {
+                LvReq = 19,
+                Cost = 16,
+            },
+			{
+                LvReq = 21,
+                Cost = 0,
+            },
+			{
+                LvReq = 24,
+                Cost = 0,
+            }
+        }
+    },
+	[65] = {
+        Name = "进阶召唤",
+        Desc = "",
+        Passive = 0,
+        Job = 2,
+        Colldown = 0,
+        Increase = 0,
+        Multiple = 0,
+        Lvs = {
+            {
+                LvReq = 40,
+                Cost = 34,
+            },
+			{
+                LvReq = 42,
+                Cost = 0,
+            },
+			{
+                LvReq = 44,
+                Cost = 0,
+            }
+        }
+    },
+	[66] = {
+        Name = "神兽天御",
+        Desc = "",
+        Passive = 0,
+        Job = 2,
+        Colldown = 0,
+        Increase = 0,
+        Multiple = 0,
+        Lvs = {
+            {
+                LvReq = 42,
+                Cost = 34,
+            },
+			{
+                LvReq = 44,
+                Cost = 0,
+            },
+			{
+                LvReq = 46,
+                Cost = 0,
+            }
+        }
+    },
+	[67] = {
+        Name = "先天气功",
+        Desc = "",
+        Passive = 0,
+        Job = 2,
+        Colldown = 0,
+        Increase = 0,
+        Multiple = 0,
+        Lvs = {
+            {
+                LvReq = 44,
+                Cost = 36,
+            },
+			{
+                LvReq = 46,
+                Cost = 0,
+            },
+			{
+                LvReq = 48,
+                Cost = 0,
+            },
+			{
+                LvReq = 62,
+                Cost = 36,
+            },
+			{
+                LvReq = 68,
+                Cost = 0,
+            },
+			{
+                LvReq = 74,
+                Cost = 0,
+            }
+        }
+    },
+	[68] = {
+        Name = "召唤白虎",
+        Desc = "",
+        Passive = 0,
+        Job = 2,
+        Colldown = 0,
+        Increase = 0,
+        Multiple = 0,
+        Lvs = {
+            {
+                LvReq = 50,
+                Cost = 40,
+            },
+			{
+                LvReq = 53,
+                Cost = 0,
+            },
+			{
+                LvReq = 55,
+                Cost = 0,
+            },
+			{
+                LvReq = 57,
+                Cost = 40,
+            },
+			{
+                LvReq = 59,
+                Cost = 0,
+            },
+			{
+                LvReq = 62,
+                Cost = 0,
+            }
+        }
+    },
+	[69] = {
+        Name = "群体治愈术",
+        Desc = "",
+        Passive = 0,
+        Job = 2,
+        Colldown = 0,
+        Increase = 8,
+        Multiple = 22,
+        Lvs = {
+            {
+                LvReq = 33,
+                Cost = 24,
+            },
+			{
+                LvReq = 35,
+                Cost = 0,
+            },
+			{
+                LvReq = 38,
+                Cost = 0,
+            }
+        }
+    },
+	[70] = {
+        Name = "绝命毒雾",
+        Desc = "",
+        Passive = 0,
+        Job = 2,
+        Colldown = 0,
+        Increase = 4,
+        Multiple = 20,
+        Lvs = {
+            {
+                LvReq = 52,
+                Cost = 28,
+            },
+			{
+                LvReq = 54,
+                Cost = 0,
+            },
+			{
+                LvReq = 56,
+                Cost = 0,
+            }
+        }
+    },
+	[71] = {
+        Name = "召唤月灵",
+        Desc = "",
+        Passive = 0,
+        Job = 2,
+        Colldown = 0,
+        Increase = 0,
+        Multiple = 0,
+        Lvs = {
+            {
+                LvReq = 62,
+                Cost = 60,
+            },
+			{
+                LvReq = 65,
+                Cost = 0,
+            },
+			{
+                LvReq = 68,
+                Cost = 0,
+            }
+        }
+    },
+}
